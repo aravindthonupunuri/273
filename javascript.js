cf42b0ea71b0699b988a4a1904cf1792c6f0aa3b
aravindthonupunuri <aravindthonupunuri75@gmail.com>
2022-02-01 10:21:07 -0800

    arrow functions split slice includes typof json.stringfy json.parse

---
+++ javascript.js
@@ -0,0 +1,32 @@
+"use strict"
+var employeeName = "aravind thonupunuri";
+var employeeEmail = "aravind@meta.com";
+var age = 23;
+// split using specific character
+var arrayOfFirstNameAndLastName = employeeName.split(" ");
+const employeeObj = {
+    names : arrayOfFirstNameAndLastName,
+    age: age
+}
+// use typeof to verify age as number
+const checkAge = (age) => {
+     if(typeof age == "number") {
+         console.log("Age verified!, Good to go");
+     }
+}
+// use slice to break the string from the index of given character
+const checkEmail = (email) => {
+    var companyName = employeeEmail.slice(employeeEmail.lastIndexOf('@'));
+    console.log(companyName);
+    if(companyName.includes("meta.com")) console.log("email verified");
+} 
+console.log(arrayOfFirstNameAndLastName);
+checkAge(age);
+checkEmail(employeeEmail);
+// parse json object to string
+const employeeObjString = JSON.stringify(employeeObj);
+console.log(employeeObjString);
+// convert string to json Object
+const employeeDetails = JSON.parse(employeeObjString);
+console.log(employeeDetails);
+console.log((employeeDetails.names)[1]);
